586f004f240590a619161a5af93674eb64bb187c
subhrajeet dash <subhrajeetdash900@gmail.com>
2025-09-25 21:11:28 +0530

    Update 2DArray.java

--- 2DArray.java
+++ 2DArray.java
@@ -37,6 +37,46 @@ public class MatrixAddition {
     }
 }
 
+// multiplication of two matrix use of 2D array
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        // First 3x3 matrix
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Second 3x3 matrix
+        int[][] matrix2 = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        // Resultant matrix
+        int[][] product = new int[3][3];
+
+        // Multiplying the matrices
+        for (int i = 0; i < 3; i++) {            // row of matrix1
+            for (int j = 0; j < 3; j++) {        // column of matrix2
+                product[i][j] = 0;
+                for (int k = 0; k < 3; k++) {    // multiply row & column
+                    product[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        // Printing the result
+        System.out.println("Product of the two matrices:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(product[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
 
 
 
